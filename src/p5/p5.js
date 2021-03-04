49dcb9069cbc2743d72990e2fbfc2625b195aa3a
Nicolò Maffi <71829227+nikmaffi@users.noreply.github.com>
2021-03-04 13:35:35 +0100

    Add files via upload

--- src/p5/p5.js
+++ src/p5/p5.js
@@ -1,6 +1,25 @@
 let player, food;
+let video, poseNet, pose;
+
+function initPoseNet() {
+    video = createCapture(VIDEO);
+    video.size(320, 200);
+    
+    poseNet = ml5.poseNet(this.video, () => { console.log('PoseNet ready!'); });
+    
+    //video.hide();
+    
+    poseNet.on('pose', (poses) => {
+        console.log(poses);
+        if(poses.length > 0) {
+            pose = poses[0].pose;
+        }
+    });
+}
 
 function setup() {
+    initPoseNet();
+    
     let cnv = createCanvas(windowWidth / 2, windowHeight);
     cnv.position((windowWidth - width) / 2, (windowHeight - height) / 2);
     
@@ -8,14 +27,18 @@ function setup() {
     food = new HotDogs(5, 10, width, height);
 }
 
-function draw() {
+function draw() {    
     background(0);
     
+    if(pose) {
+        fill(255, 0, 0);
+        ellipse(pose.nose.x, pose.nose.y, 16);
+    }
+    
     player.draw();
-    if(keyIsDown(LEFT_ARROW)) {
+    if(false) {
         player.moveLeft();
-    } 
-    if(keyIsDown(RIGHT_ARROW)) {
+    } else if(false) {
         player.moveRight();
     }
     
