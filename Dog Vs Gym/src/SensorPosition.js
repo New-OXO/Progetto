b8c1135f1dcb188447407f75650a5c190d39be78
Gabriele Ferrero <63954224+GabrieleFerrero@users.noreply.github.com>
2021-03-09 11:28:56 +0100

    Update SensorPosition.js

--- Dog Vs Gym/src/SensorPosition.js
+++ Dog Vs Gym/src/SensorPosition.js
@@ -1,4 +1,5 @@
 const RANGE_EQUAL = WIDTH/12;
+const RANGE_CENTER = WIDTH/4;
 
 
 class SensorPosition{
@@ -10,11 +11,6 @@ class SensorPosition{
         this.initPoseNet();
 
         this.posX_pre=0;
-
-        this.initSpeechRecognize();
-
-        this.speechRec;
-
     }
 
     initPoseNet() {
@@ -39,8 +35,9 @@ class SensorPosition{
     getDirection(){
         var posX = this.pose.nose.x;
         var result;
-
-        if(posX>=this.posX_pre-RANGE_EQUAL/2 && posX<=this.posX_pre+RANGE_EQUAL/2){
+        if(posX>WIDTH/2-RANGE_CENTER/2 && posX<WIDTH/2+RANGE_CENTER/2){
+            result="center_screen";
+        }else if(posX>=this.posX_pre-RANGE_EQUAL/2 && posX<=this.posX_pre+RANGE_EQUAL/2){
             result = "equal";
         }else if(posX<this.posX_pre-RANGE_EQUAL/2){
             result = "left";
@@ -50,33 +47,6 @@ class SensorPosition{
 
         this.posX_pre = posX;
         return result;
-    }
-
-
-    initSpeechRecognize(){
-        // Create a Speech Recognition object with callback
-        speechRec = new p5.SpeechRec('it', gotSpeech);
-        // "Continuous recognition" (as opposed to one time only)
-        let continuous = true;
-        // If you want to try partial recognition (faster, less accurate)
-        let interimResults = false;
-        // This must come after setting the properties
-        speechRec.start(continuous, interimResults);
-        // DOM element to display results
-        let output = select('#speech');
-        this.gotSpeech();
-    }
-
-    gotSpeech() {
-        // Something is there
-        // Get it as a string, you can also get JSON with more info
-        console.log(speechRec);
-        if (speechRec.resultValue) {
-            let said = speechRec.resultString;
-            // Show user
-            output.html(said);
-            return said;
-        }
-    }    
+    }  
 }
 
