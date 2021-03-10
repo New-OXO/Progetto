2705e669f5c5820649ef331ae2c142c869bb1316
Nicolò Maffi <71829227+nikmaffi@users.noreply.github.com>
2021-03-10 08:47:04 +0100

    Update main.js

--- Dog Vs Gym/src/main.js
+++ Dog Vs Gym/src/main.js
@@ -4,29 +4,6 @@ const HEIGHT = window.innerHeight;
 let player;
 let obstacles;
 
-//DA RIMUOVERE---------------------------------------------------------------------
-let posX
-let posX_pre = 0;
-let result;
-function getDirection() {
-    posX = mouseX;
-
-    if(posX - posX_pre > 0) {
-        result = 'RIGHT';
-    } else if(posX - posX_pre < 0) {
-        result = 'LEFT';
-    } else {
-        result = 'NONE';
-    }
-
-    player.setSpeed(Math.abs(posX - posX_pre));
-
-    posX_pre = posX;
-
-    return result;
-}
-//----------------------------------------------------------------------------------
-
 function initCanvas() {
     let cnv = createCanvas(windowWidth / 2, windowHeight);
     cnv.position((windowWidth - width) / 2, (windowHeight - height) / 2);
@@ -49,8 +26,4 @@ function draw() {
 
     player.move(getDirection());
     player.draw();
-}
-
-function keyPressed() {
-    player.grow(1);
 }
