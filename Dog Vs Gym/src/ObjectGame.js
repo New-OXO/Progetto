330ecc506896968a4910193472ad99f0b841a4da
IsabellaBianco <71838030+IsabellaBianco@users.noreply.github.com>
2021-03-11 08:43:19 +0100

    Update ObjectGame.js

--- Dog Vs Gym/src/ObjectGame.js
+++ Dog Vs Gym/src/ObjectGame.js
@@ -1,5 +1,5 @@
 class ObjectGame{
-
+//Inizializzo alcune variabili
     constructor(x, score, image, dimW, dimH){
         this.dimW = dimW;
         this.dimH = dimH;
@@ -14,7 +14,7 @@ class ObjectGame{
         this.sprite.position.y = -this.dimH/2;
         this.sprite.debug = true;
     }
-
+//Creo alcuni metodi setter e getter
     getX(){return this.x;}
 
     getY(){return this.y;}
@@ -26,14 +26,14 @@ class ObjectGame{
     getScore(){return this.score;}
 
     setScore(offset){this.score +=offset;}
-
+//Disegno lo sprite, l'immagine che ho passato come parametro al costruttore e aggiorno il punteggio
     draw(){
         imageMode(CORNER);
         image(this.image, this.x, this.y, this.dimW, this.dimH);
         drawSprite(this.sprite);
         this.updateScore(); 
     }
-
+//Se y non è minore dell'altezza della finestra ritorna false altrimenti "abbassa" l'ostacolo di speed
     move(){
         if(this.y < HEIGHT) {
             this.sprite.position.y += this.speed;
@@ -43,19 +43,19 @@ class ObjectGame{
             return false;
         }
     }
-
+//Setto la texture dell'oggetto
     setTexture(){
         this.sprite = createSprite(0, 0, this.dimW, this.dimH);
         //this.sprite.setCollider("rectangle", 0, 0, this.dimW, this.dimH);
         this.sprite.visible = false;
     }
-    
+    //"Disegno lo score
     updateScore(){
         fill(0, 0, 0);
         textSize(this.dimH);
         text(this.score, this.x+this.dimW/4, this.y+this.dimH);
     }
-
+//La funzione is hit verifica che il punteggio sia a 0.
     isHit(){
         if(this.score==0){
             return true;
