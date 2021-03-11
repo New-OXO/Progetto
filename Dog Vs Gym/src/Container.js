f5a9c84117e2f081afd2d7aa94412f981ff0d0c9
IsabellaBianco <71838030+IsabellaBianco@users.noreply.github.com>
2021-03-11 08:39:31 +0100

    Update Container.js

--- Dog Vs Gym/src/Container.js
+++ Dog Vs Gym/src/Container.js
@@ -1,3 +1,4 @@
+//Definisco alcune costanti
 const NUMBER_MAX_OBSTACLE = 10;
 const NUMBER_MIN_OBSTACLE = 0;
 const AVERAGE_NUMBER_OBSTACLE = 2;
@@ -9,6 +10,12 @@ const SCORE_MAX_OB = 50;
 const SCORE_MAX_HD = 10;
 
 class Container{
+    /*Nel costruttore vengono passati tre parametri:
+        - la probabilità che ci sia una fila piena di ostacoli
+        - la probabilità che non siano presenti ostacoli in una riga
+        - la probabilità che non ci siano hotdog
+        Settando diversamente queste variabili si può modificare la difficoltà del gioco.
+    */
     constructor(probability_max_obstacles, probability_zero, probability_zero_hotdog){
         this.probability_max_obstacles = probability_max_obstacles;
         this.probability_zero = probability_zero;
@@ -17,25 +24,28 @@ class Container{
         this.coordinate_x  = new Array();
         this.index=0;
     }
-
+    
+    //Questo metodo estrae a sorte un numero intero tra min e max.
     getRandomNumber(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-
+    
+    //Istanzio n_random Obstacle
     newSetOfObstacle(n_random, score_dog){
         for(let i=0; i<n_random; i++){
             this.array_object.push(new Obstacle(this.calculatePosition(), this.getScoreOb(i, score_dog)));
         }
     }
-
+    //Istanzio n_random Hotdog
     newSetOfHotdog(n_random, score_dog){
         for(let i=0; i<n_random; i++){
-            this.array_object.push(new Hotdog(this.calculatePosition(), this.getScoreHd(n_random, score_dog)));
+            this.array_object.push(new Hotdog(this.calculatePosition(), this.getScoreHd()));
         }
     }
-
+    
+    //Sposto sia gli ostacoli che gli hotdog di speed in basso e se è fuori dallo schermo viene eliminato
     move(){
         for(let i=0; i<this.array_object.length; i++){
             if(!this.array_object[i].move()){
@@ -45,50 +55,61 @@ class Container{
             }
         }
     }
-
+    //Genero in maniera randomica la nuova riga
     generate(score_dog){
+        //Visto che la SPEED è minore dell'altezza di un ostacolo, solo quando l'indice è SPEED-1 si creano nuovi elementi
         if(this.index==SPEED-1){
             this.coordinate_x = [];
             var n_random_ob, n_random_hd = 0;
+            //Estraggo un numero a sorte e verifico se è minore di probability_max_obstacles
             if(Math.random() < (this.probability_max_obstacles/100)){
+                //Se è minore la riga è piena 
                 n_random_ob = NUMBER_MAX_OBSTACLE;
             }else{
+                //Altrimenti etraggo a sorte la probabilità che non ci siano ostacoli
                 if(Math.random() < (this.probability_zero/100)){
                     n_random_ob = 0;
                 }
-                else{
+                else{ //e nel caso sia maggiore di probability_zero verranno creati un numero tra MINOBSTACLE e AVERAGEOBSTACLE
                     n_random_ob = this.getRandomNumber(NUMBER_MIN_OBSTACLE, AVERAGE_NUMBER_OBSTACLE);
-                }
+                }//Nel caso in cui la linea degli ostacoli non è piena estraggo anche un numero di hotdog.
                 if(Math.random() > (this.probability_zero_hotdog/100)){
                     n_random_hd = this.getRandomNumber(NUMBER_MIN_HOTDOG, NUMBER_MAX_HOTDOG);
                 }
             }
+            //Istanzio n_oggetti estratti
             this.newSetOfObstacle(n_random_ob, score_dog);
             this.newSetOfHotdog(n_random_hd, score_dog);
+            //Riporto l'index  a 0
             this.index=0;
         }else{
+            //Altrimenti incremento l'indice
             this.index++;
         }
     }
-
+    
+    //Estraggo a sorte il punteggio degli ostacoli
     getScoreOb(num_obstacle, score_dog){
         if(num_obstacle==NUMBER_MAX_OBSTACLE-1){
+            //Nel caso ci sia la linea piena, estraggo in modo che ci sia sicuramente un blocco che il bassotto possa attraversare
             return this.getRandomNumber(SCORE_MIN, score_dog-1);
         }else{
+            //Altrimenti vengono estratti casualmente tra un range prefissato
             return this.getRandomNumber(SCORE_MIN, SCORE_MAX_OB);
         }
     }
-
+    
+//Estrae a sorta un numero tra MIN e max
     getScoreHd(){
         return this.getRandomNumber(SCORE_MIN, SCORE_MAX_HD);
     }
-
+    //Disegna i vari oggetti, richiamandone il metodo draw
     draw(){
         for(let i=0; i<this.array_object.length; i++){
             this.array_object[i].draw();
         }
     }
-
+    //Calcola la posizione degli ostacoli e degli hotdog all'interno della riga e verifico che non si sovrappongano
     calculatePosition(){
         var condizione=true;
         var x;
@@ -104,7 +125,7 @@ class Container{
         this.coordinate_x.push(x);
         return x;
     }
-
+    //Verifico se il cane ha colpito qualche ostacolo, se lo score dell'ostacolo è <= 0 elimino l'ostacolo
     isHit(dog_x, dog_y){
         for(let i=0; i<this.array_object.length; i++){
             if(this.array_object[i].isHit(dog_x, dog_y)){
@@ -116,7 +137,7 @@ class Container{
         }
         return false;
     }
-
+    //Verifico che c'è stata una collisione tra dog e objectgame
     colliding(dog) {
         for(let i = 0; i < this.array_object.length; i++){
             this.array_object[i].colliding(dog);
